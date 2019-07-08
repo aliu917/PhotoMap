beadeb4bf0cd6be183cf71f322cbd454957f9679
Angela Liu <aliu18@fb.com>
2019-07-08 15:33:40 -0700

    Initial commit

---
+++ Instagram/Instagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Instagram
+//
+//  Created by aliu18 on 7/8/19.
+//  Copyright © 2019 aliu18. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Instagram/Instagram/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Instagram
+//
+//  Created by aliu18 on 7/8/19.
+//  Copyright © 2019 aliu18. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
